355a5dd42c2d8e392a4b3eb7b3def2fd6fe24ff6
Michael Fuget <michaelfuget@gmail.com>
2020-12-04 05:58:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,7 @@
 class Transfer
 
   attr_accessor :sender, :receiver, :status, :amount
-  def intialize(sender, receiver, amount)
+  def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
     @status = "pending"
