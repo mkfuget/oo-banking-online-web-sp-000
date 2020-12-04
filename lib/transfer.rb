0d4bfb00ad140c7c47c21c7dc2051784a5829ad1
Michael Fuget <michaelfuget@gmail.com>
2020-12-04 05:55:26 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,16 @@
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :status
+  def intiialize(sender, receiver)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+  end 
+  
+  def valid? 
+    rerutn @sender.valid? && @receiver.valid? 
+  end 
+  
+  
+
 end
