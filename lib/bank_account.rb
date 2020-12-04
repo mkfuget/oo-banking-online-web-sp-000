5ec4449657bc959fbbe93817e8a986a3addc839b
Michael Fuget <michaelfuget@gmail.com>
2020-12-04 05:47:54 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,4 +1,22 @@
 class BankAccount
-  attr_accessor :name 
-  def initialize 
+  attr_accessor :name, :balance, :status
+  def initialize(name) 
+    @name = name 
+    @balance = 1000
+    @status = open
+  end 
+  
+  def deposit(amount)
+    @balance += amount 
+  end 
+  
+  def display_balance 
+    puts @balance 
+  end 
+  
+  def valid? 
+    return status == open 
+  end
+  
+  
 end
