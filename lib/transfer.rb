98bab2bd293ad280bdcd91251560de56373da351
Michael Fuget <michaelfuget@gmail.com>
2020-12-04 06:03:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,9 @@ class Transfer
   def execute_transaction 
     if(@amount < sender.balance)
       sender.balance -= @amount
-      receiver.balance +=
+      receiver.balance += @amount 
+    end 
+  end
   
   
 
