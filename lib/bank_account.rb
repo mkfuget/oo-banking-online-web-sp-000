99d607713a240d8ee19007a8a5ffe41e6dc41126
Michael Fuget <michaelfuget@gmail.com>
2020-12-04 06:31:50 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,5 +1,6 @@
 class BankAccount
   attr_accessor :balance, :status
+  attr_reader :name
   def initialize(name, balance = 1000, status = "open") 
     @name = name 
     @balance = 1000
@@ -22,7 +23,5 @@ class BankAccount
     @status = "closed" 
   end 
   
-  private 
-  attr_accessor :name
-  
+
 end
